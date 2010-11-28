e551a501611b4130b7c48294f7ca8a2031ddf5fb
hbenmansour <mohamedhamza.benmansour@gmail.com>
2010-11-28 16:23:28 +0100

    project init

---
+++ gameoflife/src/test/java/fr/xebia/training/jbehave/part1/P1TestBoardStep.java
@@ -0,0 +1,26 @@
+package fr.xebia.training.jbehave.part1;
+
+import fr.xebia.training.tdd.gameoflife.Universe;
+import junit.framework.Assert;
+
+public class P1TestBoardStep {
+
+	private Universe board;
+	
+	
+	// FIXME : annoter la méthode pour en faire un step de type Given
+	public void givenAGame(int size) {
+		// FIXME : créer une nouvelle board vide, de taille size
+	}
+		
+	// FIXME : annoter la méthode pour en faire un step de type When
+	public void whenPlayARound() {
+		// FIXME : updater la board
+	}
+		
+	// FIXME : annoter la méthode pour en faire un step de type Then
+	public void thenNoCellIsAlive() {
+		// FIXME : Faire l'assertion correspondant au fait qu'aucune cellule n'est vivante
+	}	
+	
+}
---
+++ gameoflife/src/test/java/fr/xebia/training/jbehave/part3/P3TestBoardStep.java
@@ -0,0 +1,78 @@
+package fr.xebia.training.jbehave.part3;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import junit.framework.Assert;
+
+import org.jbehave.core.annotations.Alias;
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+import org.jbehave.core.model.ExamplesTable;
+
+import fr.xebia.training.tdd.gameoflife.Board;
+import fr.xebia.training.tdd.gameoflife.Coordinate;
+import fr.xebia.training.tdd.gameoflife.Universe;
+
+public class P3TestBoardStep {
+
+	private Universe board;
+	private int size;
+	private Set<Coordinate> initialStates = new HashSet<Coordinate>();
+	
+	
+	@Given("an empty board of size $size") 
+	public void givenAGame(int size) {
+		this.size = size;
+	}
+	
+	@Given("with a living cell at position $x:$y")
+	public void givenACellAtPosition(int x, int y) {
+		initialStates.add(new Coordinate(x, y));
+	}
+	
+	
+	// FIXME : ajouter une annotation @Given utilisant une classe ExamplesTable
+	public void givenInitialPositionTable(ExamplesTable table) {
+		// FIXME : insérer les initial states en bouclant sur la table d'example
+		List<Map<String,String>> rows = table.getRows();
+	}
+	
+	
+	
+	@When("we play the first round")
+	public void whenPlayARound() {
+		this.board = new Board(this.size,  getInitialStatesAsArray() );
+		board.update();
+	}
+	
+	@Then("no cell is alive")
+	public void thenNoCellIsAlive() {
+		Assert.assertEquals(0, board.liveCellsCount());
+	}
+	
+	// FIXME : annoter la méthode avec @Then
+	// FIXME : annoter la méthode avec @Alias pour faire marcher toute les stories
+	public void thenThereIsCellsAlive(int nbOfAliveCells) {
+		// FIXME : ajouter l'assertion sur le nombre de cellules vivantes
+	}
+
+	
+	private Coordinate[] getInitialStatesAsArray() {
+		Coordinate[] coordinates = new Coordinate[initialStates.size()];
+		int i = 0;
+		for (Coordinate aCoordinate : initialStates) {
+			coordinates[i++] = aCoordinate;
+		}
+		return coordinates;
+	}
+	
+	// FIXME : annoter la méthode avec @BeforeStory et réinitialiser le initial state
+	public void beforeStory() {
+	}
+
+	
+}
